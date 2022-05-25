5acb124306f854b43733ca3de8208b915cc1d4e8
HenriqueShoiti <49777312+HenriqueShoiti@users.noreply.github.com>
2022-05-24 22:24:39 -0700

    let user enter username and password for sign-in page

---
+++ components/customInput/customInput.js
@@ -0,0 +1,33 @@
+import react from 'react'
+import {View, Text, TextInput, StyleSheet, secureTextEntry} from 'react-native'
+
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
+    return(
+        <View>
+            <TextInput
+                placeholder={placeholder} 
+                value = {value}
+                onChangeText = {setValue}
+                style  = {styles.input}
+                secureTextEntry = {secureTextEntry}
+            
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'white',
+        width: '100%',
+
+        borderColor: 'e8e8e8',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginVertical:5,
+
+    },
+    input:{
+
+    },
+})
+export default CustomInput
--- screens/SigninScreen.js
+++ screens/SigninScreen.js
@@ -1,13 +1,32 @@
-import react from 'react'
+import react, {useState} from 'react'
 import { View, Text, Image, StyleSheet,useWindowDimensions } from 'react-native'
 import logo from "../assets/banana.jpg"
+import CustomInput from '../components/customInput'
 
 const SigninScreen = () => {
     const {height} = useWindowDimensions();
-    
+    const {username, setUsername} = useState('');
+    const {password, setPassword} = useState('');
+
     return (
         <View style = {styles.root}>
-            <Image source = {logo} style = {styles.logo} />
+            <Image source = {logo}
+                //make logo size 30% of the screen size
+                style = {[styles.logo, {height: height * 0.3}] }
+            />
+            
+            <CustomInput
+                placeholder="Username"
+                value={username}
+                setValue={setUsername}
+            />
+            
+            <CustomInput 
+                placeholder="Password"
+                value={password}
+                setValue={setPassword}
+                secureTextEntry
+            />
         </View>
     )
 }
